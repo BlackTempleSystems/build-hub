2fba3fdb5a939419dc5ed0e2829608c6dd7d9138
aayash <aayash@mail.com>
2026-04-28 13:43:50 +0300

    BHN-113 basic routes

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,7 +1,4 @@
 import { Routes } from '@angular/router';
-import { MainLayout } from './layouts/main-layout/main-layout';
-import { Dashboard } from './features/dashboard/dashboard';
-import { Notifications } from './features/notifications/notifications';
 import { LoginPage } from './features/authentication/login/login.page';
 import { RegisterPage } from './features/authentication/register/register.page';
 
